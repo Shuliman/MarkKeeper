030775f824cdae1b587ab3d755d1af42f703fecc
Shuliman <Julik.schuldiner@proton.me>
2023-12-22 16:51:18 +0100

    Init

---
+++ src/main/java/com/example/fulbrincjava/FulbrincJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.fulbrincjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulbrincJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FulbrincJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fulbrincjava/FulbrincJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fulbrincjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulbrincJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
