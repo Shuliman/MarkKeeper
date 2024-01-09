6f606c6c3d065ffc5d4267ba0b1a016f93080f7b
Shuliman <Julik.schuldiner@proton.me>
2024-01-09 18:09:34 +0100

    Added controller % response entity

---
+++ src/main/java/com/example/fulbrincjava/responses/LoginResponse.java
@@ -0,0 +1,18 @@
+package com.example.fulbrincjava.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Accessors(chain = true)
+@Getter
+@Setter
+public class LoginResponse {
+    private String token;
+
+    private long expiresIn;
+
+    public String getToken() {
+        return token;
+    }
+}
