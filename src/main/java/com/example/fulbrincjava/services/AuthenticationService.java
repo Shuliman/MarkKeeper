1213d51ef68bf76105ac5f8e6a4aafec7b035671
Shuliman <Julik.schuldiner@proton.me>
2024-01-09 17:50:16 +0100

    Added auth service

---
+++ src/main/java/com/example/fulbrincjava/services/AuthenticationService.java
@@ -0,0 +1,50 @@
+package com.example.fulbrincjava.services;
+
+import com.example.fulbrincjava.dtos.LoginUserDto;
+import com.example.fulbrincjava.dtos.RegisterUserDto;
+import com.example.fulbrincjava.entities.User;
+import com.example.fulbrincjava.repositories.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(
+            UserRepository userRepository,
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User signup(RegisterUserDto input) {
+        User user = new User()
+                .setLogin(input.getLogin())
+                .setEmail(input.getEmail())
+                .setPass(passwordEncoder.encode(input.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginUserDto input) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+
+        return userRepository.findByLogin(input.getEmail())
+                .orElseThrow();
+    }
+}
