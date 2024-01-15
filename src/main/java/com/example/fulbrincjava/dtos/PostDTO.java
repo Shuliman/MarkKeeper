cd81d04be327844e040666b600daba2ed2dcab44
Shuliman <Julik.schuldiner@proton.me>
2024-01-15 18:09:56 +0100

    Added DTO and Repository

---
+++ src/main/java/com/example/fulbrincjava/dtos/PostDTO.java
@@ -0,0 +1,12 @@
+package com.example.fulbrincjava.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostDTO {
+    private Long id;
+    private String title;
+    private String description;
+}
---
+++ src/main/java/com/example/fulbrincjava/repositories/PostRepository.java
@@ -0,0 +1,15 @@
+
+package com.example.fulbrincjava.repositories;
+
+import com.example.fulbrincjava.entities.Post;
+import com.example.fulbrincjava.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+@Repository
+public interface PostRepository extends CrudRepository<Post, Long> {
+    List<Post> findByUserId(User user);
+    Optional<Post> findById(Long id);
+}
