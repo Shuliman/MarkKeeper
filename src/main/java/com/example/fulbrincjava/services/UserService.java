7c2a279d25f071727b4f06f0b671c69bb92e5c2f
Shuliman <Julik.schuldiner@proton.me>
2024-01-19 17:19:36 +0100

    Added getCurrentUser method

--- src/main/java/com/example/fulbrincjava/services/UserService.java
+++ src/main/java/com/example/fulbrincjava/services/UserService.java
@@ -2,10 +2,13 @@ package com.example.fulbrincjava.services;
 
 import com.example.fulbrincjava.entities.User;
 import com.example.fulbrincjava.repositories.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -21,5 +24,10 @@ public class UserService {
         userRepository.findAll().forEach(users::add);
 
         return users;
+    }
+    public Optional<User> getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserName = authentication.getName();
+        return userRepository.findByEmail(currentUserName);
     }
 }
