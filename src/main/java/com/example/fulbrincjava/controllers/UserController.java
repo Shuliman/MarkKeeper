8522559953c88d038bd02415bfca9c1a74b9e831
Shuliman <Julik.schuldiner@proton.me>
2024-02-27 14:22:44 +0100

    Added user by id endpoint

--- src/main/java/com/example/fulbrincjava/controllers/UserController.java
+++ src/main/java/com/example/fulbrincjava/controllers/UserController.java
@@ -1,15 +1,18 @@
 package com.example.fulbrincjava.controllers;
 
 import com.example.fulbrincjava.entities.User;
+import com.example.fulbrincjava.exceptions.UserNotFoundException;
 import com.example.fulbrincjava.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/users")
 @RestController
@@ -35,4 +38,12 @@ public class UserController {
 
         return ResponseEntity.ok(users);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> userById(@PathVariable Integer id) {
+        User user = userService.getUserById(id);
+
+        return ResponseEntity.ok(user);
+    }
+
 }
--- src/main/java/com/example/fulbrincjava/services/UserService.java
+++ src/main/java/com/example/fulbrincjava/services/UserService.java
@@ -1,6 +1,10 @@
 package com.example.fulbrincjava.services;
 
+import com.example.fulbrincjava.dtos.PostDTO;
+import com.example.fulbrincjava.entities.Post;
 import com.example.fulbrincjava.entities.User;
+import com.example.fulbrincjava.exceptions.PostNotFoundException;
+import com.example.fulbrincjava.exceptions.UserNotFoundException;
 import com.example.fulbrincjava.repositories.UserRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -30,4 +34,10 @@ public class UserService {
         String currentUserName = authentication.getName();
         return userRepository.findByEmail(currentUserName);
     }
+
+    public User getUserById(Integer id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User not found with id " + id));
+    }
+
 }
